bb9d078c41e4d1ac49e670c2bf037b74e1df7722
somya51p <somya2001p@gmail.com>
2021-06-27 10:43:38 +0530

    text editor added

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./SendMail.css";
 import CloseIcon from "@material-ui/icons/Close";
 import { Button } from "@material-ui/core";
-import TextEditor from "./TextEditor";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
 
 function SendMail({ setShowEditor, showEditor }) {
   const onSubmit = (data) => {};
+  const [editorState, setEditorState] = useState("");
+  // state = {
+  //   editorState: EditorState.createEmpty(),
+  // };
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+  
+  // const { editorState } = this.state;
 
   if (!showEditor) {
     return null;
@@ -26,13 +39,23 @@ function SendMail({ setShowEditor, showEditor }) {
         <input name="cc" placeholder="Cc" type="email" />
         <input name="bcc" placeholder="bcc" type="email" />
         <input name="subject" placeholder="Subject" type="text" />
+        <div>
         <input
           name="message"
           className="sendMail__message"
           placeholder="Message..."
           type="text"
+          // value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
         />
-        <TextEditor />
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={onEditorStateChange}
+        />
+        </div>
+        
         <div className="sendMail__options">
           <Button
             className="sendMail__send"
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState, convertToRaw } from "draft-js";
-
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
 
